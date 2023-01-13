79999e905fcac6d2f124f2b6e46ad6ea661d73e6
Ritish Shailly <rshailly.vt.edu>
2023-01-13 06:33:11 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+author: Ritish Shailly
+
+This repository was made to understand the launch file in ROS better.
+Here is the video link I followed:
+https://www.youtube.com/watch?v=0-zTU4_S6vY
+
+
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ritish/luqman_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/ritish/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/ritish/luqman_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ritish/luqman_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ritish/luqman_ws/src:$ROS_PACKAGE_PATH"
